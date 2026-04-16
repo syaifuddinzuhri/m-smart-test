d900d2cc2d6e9a4a6159575a597dee4c255322f4
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-04-16 21:30:06 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <meta name="robots" content="noindex, nofollow">
+    {{-- <meta name="robots" content="noindex, nofollow"> --}}
 
     <link rel="icon" href="/favicon.ico?v=2">
     <link rel="shortcut icon" href="/favicon.ico?v=2">
