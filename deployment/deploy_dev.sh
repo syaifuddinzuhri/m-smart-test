c230864187bfb4c7d5cf0cd29b86bc2cd65f9b2b
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-04-16 21:56:50 +0700

    fix

--- deployment/deploy_dev.sh
+++ deployment/deploy_dev.sh
@@ -10,12 +10,17 @@ git pull origin main
 echo "📦 Memperbarui dependensi Composer..."
 composer update --no-interaction --prefer-dist --optimize-autoloader
 
+nvm use 20
+npm install
+npm run build
+
 echo "🧹 Membersihkan dan memperbarui cache Laravel..."
 php artisan optimize:clear
 php artisan optimize
 php artisan view:cache
 php artisan config:cache
 php artisan event:cache
+php artisan view:clear
 
 echo "⚡ Menjalankan Dump Autoload..."
 composer dump-autoload -o
