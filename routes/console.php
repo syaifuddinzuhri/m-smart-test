fb9758f3fdc24627dea46558e9a2d035e39e54dd
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-04-18 22:01:58 +0700

    fix

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('exam:update-status')->everyMinute();
 Schedule::command('exam:clear-tokens')->everyMinute();
+Schedule::command('exam:close-expired-sessions')->everyMinute();
 
 Schedule::command('session:prune')->everyFiveMinutes();
